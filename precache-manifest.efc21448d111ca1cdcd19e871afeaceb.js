186d27cf5b24d7118ce689c74784696fe84acd99
MishkaGolota <golotamm@gmail.com>
2019-05-05 11:49:14 +0300

    Updates

---
+++ precache-manifest.efc21448d111ca1cdcd19e871afeaceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae2368ab9dff5796eb36",
+    "url": "/youtube-player/static/js/runtime~main.080ccfc3.js"
+  },
+  {
+    "revision": "678a45873af73a9406f9",
+    "url": "/youtube-player/static/js/main.b9227460.chunk.js"
+  },
+  {
+    "revision": "e08245dc63f624d8c29c",
+    "url": "/youtube-player/static/js/2.f9080403.chunk.js"
+  },
+  {
+    "revision": "678a45873af73a9406f9",
+    "url": "/youtube-player/static/css/main.462774e6.chunk.css"
+  },
+  {
+    "revision": "c90a42fa7ff5c4dd7c02c773eb5d70e4",
+    "url": "/youtube-player/index.html"
+  }
+];
